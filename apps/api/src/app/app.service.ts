42b8eb3d02cc3c074e25e37101503fe57c49a579
Narek Vardanjan <vnarek@gmail.com>
2022-08-05 14:16:45 +0200

    Update app.service.ts

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Welcome to fapi!' };
+    return { message: 'Welcome to graapi!' };
   }
 }
